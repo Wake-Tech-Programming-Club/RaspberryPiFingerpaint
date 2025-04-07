97c11643ad7b90297f6607768f3447468bc5a71d
BobbyTheCatfish <alpusey@my.waketech.edu>
2025-04-07 18:29:31 -0400

    smaller camera, new config

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import cv2
 
 def calc_window_size(config: ConfigParser, cam_width: int, cam_height: int):
     screen_width = config.getint("screen", "width")
-    screen_height = config.getint("screen", "height")
+    screen_height = min(config.getint("screen", "height"), config.getint("camera", "max_height"))
 
     cam_aspect_ratio = cam_width / cam_height
     screen_aspect_ratio = screen_width / screen_height
